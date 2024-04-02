1ab9bf7d2c1533f7be915ce1b2b591ab574dcd6d
Xiaosea <maysrp@gmail.com>
2024-04-02 18:16:48 +0800

    Update boot.py

--- src/boot.py
+++ src/boot.py
@@ -1,5 +1,18 @@
-from machine import Pin
+from machine import Pin,freq
+from config import config
+import shell as sh 
 ok=Pin(8,Pin.IN,Pin.PULL_UP)
+freq(240000000)
+def shell():
+    con.cls()
+    sh.shell()
+    
 if ok.value():
     import start
+    start.scr.bgcolor=0
+    con=config(start)
+    con.windows()
+
+
+
 
