3fe26bc60c706eb517518d869e4be6c2d04ff653
Xiaosea <maysrp@gmail.com>
2024-04-04 17:57:09 +0800

    Update boot.py

--- src/boot.py
+++ src/boot.py
@@ -4,14 +4,25 @@ import shell as sh
 ok=Pin(8,Pin.IN,Pin.PULL_UP)
 freq(240000000)
 def shell():
+    con.green()
+    con.small()
     con.cls()
     sh.shell()
     
+
+bl=Pin(16,Pin.OUT)
+def light(t):
+    if bl.value():
+        bl.value(0)
+    else:
+        bl.value(1)
+    
 if ok.value():
+    ok.irq(trigger=Pin.IRQ_RISING, handler=light)
     import start
     start.scr.bgcolor=0
     con=config(start)
-    con.windows()
+    con.green()
 
 
 
